5e84e2943acabb977a8b8c7e51aa34bd531a333a
Abir Hossain <abirhossain0329@gmail.com>
2026-07-26 02:18:41 +0600

    Sync Codeforces: 1840C - Ski Resort [C++17 (GCC 7-32)]

---
+++ 1840C_Ski_Resort.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+void solve()
+{
+    ll n,k,q;
+    cin>>n>>k>>q;
+    vector<ll> arr(n);
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    arr.push_back(q + 1);
+    ll conse_cnt =0;
+    ll ans =0;
+    for(int i=0;i<n+1;i++)
+    {
+        if(arr[i]<=q)
+        {
+            conse_cnt++;
+        }
+        else
+        {
+            if(conse_cnt>=k)
+            {
+                ll d = conse_cnt - k + 1;
+                ans+= d*(d+1)/2;
+            }
+            conse_cnt = 0;
+        }
+    }
+    cout<< ans <<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll tc;
+    cin>>tc;
+    while(tc--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
