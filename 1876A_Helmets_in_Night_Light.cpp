0f75a6ca15a2d1b167fbfc0c66ab23685ba4c97a
Abir Hossain <abirhossain0329@gmail.com>
2026-09-24 16:34:35 +0600

    Sync Codeforces: 1876A - Helmets in Night Light [C++17 (GCC 7-32)]

--- 1876A_Helmets_in_Night_Light.cpp
+++ 1876A_Helmets_in_Night_Light.cpp
@@ -27,7 +27,12 @@ void solve()
         viki[i].first = B[i];
         viki[i].second = A[i];
     }
-    sort(viki.begin(), viki.end());
+    sort(viki.begin(), viki.end(), [](pair<ll,ll> &a, pair<ll,ll> &b){
+        if(a.first== b.first){
+            return a.second>b.second;
+        }
+        return a.first<b.first;
+    });
     ll ans = p;
     ll j = 0;
     ll cnt = n - 1;
