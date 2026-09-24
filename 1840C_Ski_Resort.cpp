9d69d2025645003ec09f25cfb6f04dfa15210c36
Abir Hossain <abirhossain0329@gmail.com>
2026-09-24 16:35:06 +0600

    Sync Codeforces: 1840C - Ski Resort [C++17 (GCC 7-32)]

--- 1840C_Ski_Resort.cpp
+++ 1840C_Ski_Resort.cpp
@@ -1,34 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 using namespace std;
  
 void solve()
 {
-    ll n,k,q;
-    cin>>n>>k>>q;
+    ll n, k, q;
+    cin >> n >> k >> q;
     vector<ll> arr(n);
-    for(int i=0;i<n;i++)
-    cin>>arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
     arr.push_back(q + 1);
-    ll conse_cnt =0;
-    ll ans =0;
-    for(int i=0;i<n+1;i++)
+    ll conse_cnt = 0;
+    ll ans = 0;
+    for (int i = 0; i < n + 1; i++)
     {
-        if(arr[i]<=q)
+        if (arr[i] <= q)
         {
-            conse_cnt++;
+            conse_cnt++;//magic
         }
         else
         {
-            if(conse_cnt>=k)
+            if (conse_cnt >= k)
             {
                 ll d = conse_cnt - k + 1;
-                ans+= d*(d+1)/2;
+                ans += d * (d + 1) / 2;
             }
             conse_cnt = 0;
         }
     }
-    cout<< ans <<endl;
+    cout << ans << endl;
 }
  
 int main()
@@ -37,8 +37,8 @@ int main()
     cin.tie(NULL);
  
     ll tc;
-    cin>>tc;
-    while(tc--)
+    cin >> tc;
+    while (tc--)
     {
         solve();
     }
